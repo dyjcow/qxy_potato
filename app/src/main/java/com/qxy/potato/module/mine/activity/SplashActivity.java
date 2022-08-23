a588b3f5316e8e2581352ab92fc7c62ce518c81a
dyjcow <Dyj15113657804@outlook.com>
2022-08-23 22:13:03 +0800

    update(*):取消启动页面的延迟，修改错别字

--- app/src/main/java/com/qxy/potato/module/mine/activity/SplashActivity.java
+++ app/src/main/java/com/qxy/potato/module/mine/activity/SplashActivity.java
@@ -3,7 +3,6 @@ package com.qxy.potato.module.mine.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.os.Handler;
 
 import com.qxy.potato.R;
 import com.qxy.potato.module.home.activity.HomeActivity;
@@ -15,6 +14,7 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        new Handler().postDelayed(() -> ActivityUtil.startActivity(HomeActivity.class,true),1000);
+        ActivityUtil.startActivity(HomeActivity.class,true);
+
     }
 }
