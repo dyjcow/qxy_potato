6184ffd98d8df85aae64cc884adef5b944806cd2
dyjcow <Dyj15113657804@outlook.com>
2022-09-07 22:16:50 +0800

    fix(*):由于职员信息为空导致的崩溃 #22

--- app/src/main/java/com/qxy/potatos/module/videorank/adapter/VideoRankAdapter.kt
+++ app/src/main/java/com/qxy/potatos/module/videorank/adapter/VideoRankAdapter.kt
@@ -1,25 +1,19 @@
-package com.qxy.potatos.module.videorank.adapter;
+package com.qxy.potatos.module.videorank.adapter
 
-import android.view.View;
-import android.widget.Toast;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
-import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
-import com.bumptech.glide.request.RequestOptions;
-import com.chad.library.adapter.base.BaseQuickAdapter;
-import com.chad.library.adapter.base.viewholder.BaseViewHolder;
-import com.dylanc.viewbinding.brvah.BaseViewHolderUtilKt;
-import com.qxy.potatos.R;
-import com.qxy.potatos.bean.VideoList;
-import com.qxy.potatos.databinding.RecyclerviewItemRankBinding;
-import com.qxy.potatos.util.ActivityUtil;
-
-import java.text.DecimalFormat;
-import java.util.List;
+import android.view.View
+import com.dylanc.viewbinding.brvah.getBinding
+import com.qxy.potatos.bean.VideoList
+import com.bumptech.glide.request.RequestOptions
+import com.qxy.potatos.R
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.Glide
+import com.qxy.potatos.util.ActivityUtil
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import android.widget.Toast
+import com.chad.library.adapter.base.BaseQuickAdapter
+import com.chad.library.adapter.base.viewholder.BaseViewHolder
+import com.qxy.potatos.databinding.RecyclerviewItemRankBinding
+import java.text.DecimalFormat
 
 /**
  * @author ：Dyj
@@ -28,49 +22,51 @@ import java.util.List;
  * @modified By：
  * @version: 1.0
  */
-public class VideoRVAdapter extends BaseQuickAdapter<VideoList.Video, BaseViewHolder> {
+class VideoRankAdapter(layoutResId: Int, data: MutableList<VideoList.Video>?) :
+    BaseQuickAdapter<VideoList.Video, BaseViewHolder>(layoutResId, data) {
 
-    public VideoRVAdapter(int layoutResId, @Nullable List<VideoList.Video> data) {
-        super(layoutResId, data);
-    }
-
-    public VideoRVAdapter(int layoutResId) {
-        super(layoutResId);
-    }
-
-    @Override
-    protected void convert(@NonNull BaseViewHolder baseViewHolder, VideoList.Video video) {
-        RecyclerviewItemRankBinding binding = BaseViewHolderUtilKt
-                .getBinding(baseViewHolder, RecyclerviewItemRankBinding::bind);
-        RequestOptions options = new RequestOptions()
-                .placeholder(R.drawable.ic_launcher_foreground)
-                .error(R.drawable.ic_launcher_foreground)
-                .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC);
+    override fun convert(holder: BaseViewHolder, item: VideoList.Video) {
+//        val binding = holder.getBinding { view: View? ->
+//            RecyclerviewItemRankBinding.bind(
+//                view!!
+//            )
+//        }
+        val binding = holder.getBinding(RecyclerviewItemRankBinding::bind)
+        val options = RequestOptions()
+            .placeholder(R.drawable.ic_launcher_foreground)
+            .error(R.drawable.ic_launcher_foreground)
+            .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
         Glide.with(ActivityUtil.getCurrentActivity())
-                .load(video.getPoster())
-                .apply(options)
-                .transition(DrawableTransitionOptions.withCrossFade())
-                .into(binding.imageViewIcon);
-        binding.textViewName.setText(video.getName());
-        binding.textViewPopDegree.setText(getNumber(video.getHot()));
-        binding.textViewReleaseTime.setText(video.getRelease_date() + " 上映");
-        if (video.getTags() == null) {
-            binding.textViewType.setText("[国语]");
+            .load(item.poster)
+            .apply(options)
+            .transition(DrawableTransitionOptions.withCrossFade())
+            .into(binding.imageViewIcon)
+        binding.textViewName.text = item.name
+        binding.textViewPopDegree.text = getNumber(item.hot)
+        binding.textViewReleaseTime.text = (item.release_date?:"未定时间") + " 上映"
+        binding.textViewType.text = item.tags?.toString()?.let { getTag(it) } ?:"国语"
+        binding.textViewScore.text = "暂无评分"
+        if (item.type == 1) {
+            binding.buttonGetTicket.setOnClickListener { v: View ->
+                Toast.makeText(
+                    v.context,
+                    "无法购票",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
         } else {
-            binding.textViewType.setText(video.getTags() + "");
-        }
-        binding.textViewScore.setText("暂无评分");
-        if (video.getType() == 1) {
-            binding.buttonGetTicket.setOnClickListener(
-                    v -> Toast.makeText(v.getContext(), "无法购票", Toast.LENGTH_SHORT).show());
-        } else {
-            binding.buttonGetTicket.setVisibility(View.GONE);
+            binding.buttonGetTicket.visibility = View.GONE
         }
     }
 
-    private String getNumber(int num) {
-        float n = (float) num / 10000;
-        DecimalFormat decimalFormat = new DecimalFormat(".00");
-        return decimalFormat.format(n) + "万";
+    private fun getNumber(num: Int): String {
+        val n = num.toFloat() / 10000
+        val decimalFormat = DecimalFormat(".00")
+        return decimalFormat.format(n.toDouble()) + "万"
+    }
+
+    private fun getTag(tag: String): String {
+        val mTag = tag.substring(1, tag.length-1)
+        return mTag.replace(",","/")
     }
 }
--- app/src/main/java/com/qxy/potatos/module/videorank/fragment/VideoRankFragment.java
+++ app/src/main/java/com/qxy/potatos/module/videorank/fragment/VideoRankFragment.java
@@ -1,13 +1,8 @@
 package com.qxy.potatos.module.videorank.fragment;
 
-import android.view.View;
-
-import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.chad.library.adapter.base.BaseQuickAdapter;
-import com.chad.library.adapter.base.listener.OnItemClickListener;
 import com.qxy.potatos.R;
 import com.qxy.potatos.annotation.BindEventBus;
 import com.qxy.potatos.base.BaseEvent;
@@ -15,14 +10,13 @@ import com.qxy.potatos.base.BaseFragment;
 import com.qxy.potatos.bean.VideoList;
 import com.qxy.potatos.bean.VideoVersion;
 import com.qxy.potatos.common.EventCode;
-import com.qxy.potatos.databinding.EmptyViewBinding;
 import com.qxy.potatos.databinding.FragmentRankBackgroundBinding;
 
 import com.qxy.potatos.module.videorank.Dialog.RankItemDialog;
 
 import com.qxy.potatos.module.videorank.activity.RankActivity;
 import com.qxy.potatos.module.videorank.adapter.MyItemDecoration;
-import com.qxy.potatos.module.videorank.adapter.VideoRVAdapter;
+import com.qxy.potatos.module.videorank.adapter.VideoRankAdapter;
 import com.qxy.potatos.module.videorank.presenter.VideoRankPresenter;
 import com.qxy.potatos.module.videorank.view.IVideoRankView;
 import com.qxy.potatos.util.ToastUtil;
@@ -43,7 +37,7 @@ import java.util.Locale;
 public class VideoRankFragment extends BaseFragment<VideoRankPresenter, FragmentRankBackgroundBinding> implements IVideoRankView {
 
     private final int type;
-    private VideoRVAdapter rvAdapter;
+    private VideoRankAdapter rvAdapter;
     private RankActivity activity;
 
     public VideoRankFragment(int type) {
@@ -90,7 +84,7 @@ public class VideoRankFragment extends BaseFragment<VideoRankPresenter, Fragment
     public void showRankSuccess(VideoList videoList, int version) {
         //修复多次添加ItemDecoration导致的偏移问题
         if(rvAdapter == null){
-            rvAdapter = new VideoRVAdapter(R.layout.recyclerview_item_rank, videoList.getList());
+            rvAdapter = new VideoRankAdapter(R.layout.recyclerview_item_rank, videoList.getList());
             rvAdapter.setOnItemClickListener((adapter, view, position) -> {
                 if (activity != null){
                     RankItemDialog itemDialog = new RankItemDialog(activity.hand,videoList.getList().get(position));
@@ -127,7 +121,6 @@ public class VideoRankFragment extends BaseFragment<VideoRankPresenter, Fragment
     public void onMainActivityEvent(BaseEvent<VideoVersion.Version> event) {
         if (event.getEventCode() == EventCode.SELECT_VERSION) {
             presenter.getLastVersionRank(type, event.getData().getVersion());
-            ToastUtil.showToast(String.valueOf(event.getData().getVersion()));
         }
     }
 }
